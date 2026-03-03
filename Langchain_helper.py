49b81e5e131b4806836ca0c7be4bb58142476035
Emaduddin <emaduddinfrosty@gmail.com>
2026-03-03 23:53:29 +0400

    Added a CharacterTextSplitter to the text variable

--- Langchain_helper.py
+++ Langchain_helper.py
@@ -1,9 +1,23 @@
 from langchain_community.document_loaders import TextLoader
 from langchain_community.document_loaders import UnstructuredURLLoader
+from langchain_text_splitters import CharacterTextSplitter
 
 loader = UnstructuredURLLoader(urls=[
     "https://www.thenationalnews.com/news/mena/2026/03/01/the-supersonic-boom-of-thaads-hit-to-kill-defences-more-100km-above-uae/"
 ])
 
 data=loader.load()
-print(data)
+
+text= """
+People underneath the cone of sound will hear a sudden crack, or boom, even if the object of the interception is very high above them.
+Soundwaves can also bend back towards the ground, with differences in air temperature and density making distant explosions travel further, generating a powerful boom effect over Abu Dhabi or Dubai.
+Some have described it as the “canyoning effect” experienced by troops in mountainous Afghanistan when the Taliban might open fire some distance away but the echoing effect of steep valleys made it sound much closer. 
+"""
+
+print(text)
+
+splitter = CharacterTextSplitter(
+    separator="/n",
+    chunk_size=200
+)
+print(splitter.split_text(text))
